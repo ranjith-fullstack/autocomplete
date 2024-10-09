dfe0503e1480f89a8ffaaa7659a98366efb52432
Ranjith Kumar <ranjithkumarreddy3108@gmail.com>
2024-10-09 12:03:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DataTable from './DataTable';
+import AutoCompleter from './AutoCompleter';
+
+function App() {
+  return (
+  //  <DataTable />
+   <AutoCompleter />
+  );
+}
+
+export default App;
